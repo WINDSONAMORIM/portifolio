a899a65f51457d6d204f997e286816ff4d1495c5
windsonamorim <windson.amorim@gmail.com>
2023-10-15 20:48:34 -0300

    feat: importa css e remove tag small

--- src/components/socialMedia/index.tsx
+++ src/components/socialMedia/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 export const SocialMedia = () => {
     const facebook = require('../../assests/icon/facebook.ico');
@@ -10,13 +11,13 @@ export const SocialMedia = () => {
     return(
         <>
             <footer>
-                <small>
+                {/* <small> */}
                     <a href="https://www.facebook.com/windson.amorim/" target="_blank"><img src={facebook} alt="" /></a>
                     <a href="https://www.instagram.com/windsonamorim/" target="_blank"><img src={instagram} alt="" /></a>
                     <a href="https://web.whatsapp.com/send?phone=5511992597337" target="_blank"><img src={whatsapp} alt="" /></a>
                     <a href="https://www.linkedin.com/in/windson-amorim-marana-8614441b3/" target="_blank"><img src={linkedin} alt="" /></a>
                     <a href="https://github.com/WINDSONAMORIM" target="_blank"><img src={github} alt="" /></a>
-                </small>
+                {/* </small> */}
             </footer>
         </>     
         )
