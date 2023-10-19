7be959e397e664e77cd24834b975d443fdad21f5
windsonamorim <windson.amorim@gmail.com>
2023-10-19 12:40:56 -0300

    feat: teste menu

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,10 +5,12 @@ import { TitleAnimado } from '../../components/titleAnimado';
 import { SocialMedia } from '../../components/socialMedia';
 import { Header } from '../../components/header';
 import { About } from '../../components/about';
+import { MenuBurguer } from '../../components/menu';
 
 export const Home =  () => {
     return(        
         <Wrapper>
+            <MenuBurguer />
             <Header>
                 <Avatar />
                 <TitleAnimado texto='Desenvolvimento Web' />
