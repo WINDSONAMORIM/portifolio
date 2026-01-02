f64666b78d087133600038defbdf6fc11b315bdd
windsonamorim <windson.amorim@gmail.com>
2026-01-02 16:32:16 -0300

    anima logo

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./style.css";
+import perfilImage from "../../assets/images/logo2.png";
 // const perfilImage = require("../../assets/images/perfil-removebg-preview.png");
 
 export const Avatar = () => {
-  const perfilImage = require("../../assets/images/logo2.png");
+  // const perfilImage = require("../../assets/images/logo2.png");
   return (
     <div className="content">
         <img className='logo' src={perfilImage} alt="logo ou foto do Perfil"/>
